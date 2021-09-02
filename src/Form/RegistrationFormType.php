5511cbe80d3e228a88c56d31110a0fe5b2081f59
unknown <Administrateur@139SE33-HXZ2KF2.ad.campus-eni.fr>
2021-09-02 14:40:24 +0200

    Correction page register

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -17,16 +17,10 @@ class RegistrationFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
+            ->add('prenom')
+            ->add('nom')
             ->add('email')
-            ->add('agreeTerms', CheckboxType::class, [
-                'mapped' => false,
-                'constraints' => [
-                    new IsTrue([
-                        'message' => 'You should agree to our terms.',
-                    ]),
-                ],
-            ])
-            ->add('plainPassword', PasswordType::class, [
+            ->add('MotDePasse', PasswordType::class, [
                 // instead of being set onto the object directly,
                 // this is read and encoded in the controller
                 'mapped' => false,
